c3501ef644c7a034371121096a86cbddd20a6bf5
Backuper <please@enjoy-jlg-backup.com>
2020-11-09 15:59:54 +0100

    added scss

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -18,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          <li>toto</li>
+        </ul>
       </header>
     </div>
   );
