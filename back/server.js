f8b29db26f7c7509d22084e24047f7605b990cb2
Backuper <please@enjoy-jlg-backup.com>
2020-11-09 12:09:11 +0100

    static server

---
+++ back/server.js
@@ -0,0 +1,20 @@
+console.log("start");
+
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+const port = 3000;
+const www = "public";
+
+app.use((req, res, next) => {
+  console.log("url", req.url);
+  next();
+});
+
+app.use(express.static(www));
+app.use(serveIndex(www, { icons: true }));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
