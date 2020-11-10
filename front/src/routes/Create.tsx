7bde9a95bef0d9620ca85f6cd1ede2ebd82afff9
Backuper <please@enjoy-jlg-backup.com>
2020-11-10 14:53:29 +0100

    css create

--- front/src/routes/Create.tsx
+++ front/src/routes/Create.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 
+import "./Create.scss";
+
 function Create() {
   return (
-    <main>
+    <main className="create">
       <h1>Ajouter un article</h1>
+      <form>
+        <label>
+          <div>Nom</div>
+          <input type="text" />
+        </label>
+        <label>
+          <div>Prix</div>
+          <input type="number" min="0" step="0.01" />
+        </label>
+        <label>
+          <div>Quantité</div>
+          <input type="number" min="0" step="1" />
+        </label>
+        <button className="primary">Ajouter</button>
+      </form>
     </main>
   );
 }
