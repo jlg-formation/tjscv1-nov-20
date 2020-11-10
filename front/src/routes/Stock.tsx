7e6bd45a821a4cec8ba840973f4935e429aa0a85
Backuper <please@enjoy-jlg-backup.com>
2020-11-10 13:56:03 +0100

    stock dom

--- front/src/routes/Stock.tsx
+++ front/src/routes/Stock.tsx
@@ -5,9 +5,42 @@ function Stock() {
   return (
     <main>
       <h1>Liste des articles</h1>
-      <Link to="/create">
-        <button>Ajouter</button>
-      </Link>
+      <nav>
+        <Link to="/create">
+          <button>Ajouter</button>
+        </Link>
+      </nav>
+      <table>
+        <thead>
+          <tr>
+            <th className="name">Nom</th>
+            <th className="price">Prix</th>
+            <th className="qty">Quantité</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="name">Tournevis</td>
+            <td className="price">1.45€</td>
+            <td className="qty">234</td>
+          </tr>
+          <tr>
+            <td className="name">Tournevis Cruciforme</td>
+            <td className="price">11.00€</td>
+            <td className="qty">10</td>
+          </tr>
+          <tr>
+            <td className="name">Tondeuse à gazon</td>
+            <td className="price">211.00€</td>
+            <td className="qty">5</td>
+          </tr>
+          <tr>
+            <td className="name">Pince</td>
+            <td className="price">0.45€</td>
+            <td className="qty">100</td>
+          </tr>
+        </tbody>
+      </table>
     </main>
   );
 }
