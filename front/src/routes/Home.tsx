330a8ac1533ba6370f61f35b5baa41d5ae99f111
Backuper <please@enjoy-jlg-backup.com>
2020-11-10 13:55:27 +0100

    add scss to home

--- front/src/routes/Home.tsx
+++ front/src/routes/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Home.scss";
+
 function Home() {
   return (
-    <main>
+    <main className="home">
       <h1>Gérer vos stocks efficacement !</h1>
       <Link to="/stock">
         <button className="primary">Voir le stock</button>
